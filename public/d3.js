ebbe30484607f1b2b4aa22e3796fbd21248e041f
Kevin Tong <kevintong@C02CJ5N6MD6M.tld>
2020-09-19 15:34:13 -0700

    Set up d3.js and added in canvas

---
+++ public/d3.js
@@ -0,0 +1,14 @@
+//d3 JavaScript
+
+const canvas = d3.select(".canva");
+canvas.attr("width", "500").attr("height", "500")
+const svg = canvas.append("svg")
+                .attr('width', "500px")
+                .attr("height", "250px")
+
+svg.append("circle").attr("cx", "134")
+                .attr("cy", "134")
+                .attr("r", "50")
+                .attr("fill", "none")
+                .attr("stroke", "yellow")
+                .attr("stroke-width", "5px")
