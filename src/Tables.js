f982a3c4de98adce3b94cee028d1c9d4c9827433
Kevin Tong <kevintong@C02CJ5N6MD6M.tld>
2020-09-19 20:39:56 -0700

    Completed Tables page (might add search later and/or filter)

--- src/Tables.js
+++ src/Tables.js
@@ -14,10 +14,15 @@ class Tables extends React.Component{
     constructor(props){
         super(props)
         this.state = {
+            current: "na",
+            index: 0,
+            global: false,
             countries: {
-                Countries: [],
+                Countries: [{"Country": "n/a", "Date": "n/a", "NewConfirmed": "n/a", "NewDeaths": "n/a", "NewRecovered": "n/a"}],
+                Date: "2020-02-10gdsgds",
             },
         }
+        this.handleChange = this.handleChange.bind(this)
     }
 
     componentDidMount(){
@@ -25,30 +30,94 @@ class Tables extends React.Component{
             .then((data) => data.json())
             .then((data) => {
                 this.setState({
+                    current: "na",
+                    index: 2,
+                    global: false,
                     countries: data,
                 })
             })
     }
 
-    render(){
-        let arr = [1,2,3,4,5]
-        for(let i =0; i<400; i++){
-            arr.push(i)
+    handleChange(){
+        let newVal = document.querySelector("#countries").value
+        let newIndex = 0;
+        for(let i =0 ;i<this.state.countries.Countries.length; i++){
+            if(this.state.countries.Countries[i].CountryCode === newVal){
+                newIndex = i
+                break
+            }
         }
-        console.log(arr)
-        //console.log(this.state.countries.Countries)
+        this.setState((prevState)=>{
+            return {
+                current: newVal,
+                index:  newIndex,
+                global: newVal === 'global',
+                countries: prevState.countries
+            }
+        })
+    }
+
+    dateToText(date){
+        let year = date.substr(0,4)
+        let month = date.substr(5,2)
+        let day = date.substr(8, 2)
+        switch(month){
+            case "01": month = "January"; break;
+            case "02": month = "February"; break;
+            case "03": month = "March"; break;
+            case "04": month = "April"; break;
+            case "05": month = "May"; break;
+            case "06": month = "June"; break;
+            case "07": month = "July"; break;
+            case "08": month = "August"; break;
+            case "09": month = "September"; break;
+            case "10": month = "October"; break;
+            case "11": month = "November"; break;
+            case "12": month = "December"; break;
+            default: month = "Invalid"; break;
+        }
+        return month + " " + day + ", " + year
+    }
+
+    render(){
+        console.log(this.state)
         return(
             <div className = "tables-container">
-                <h1>Tables</h1>
+                <h1>COVID Tables</h1>
                 <label>Please Select a Country to view data for:  </label>
-                <select name = "countries" id = "countries">
-                    <option selected = "selected">Select a Country</option>
+                <select name = "countries" id = "countries" onChange = {this.handleChange}>
+                    <option value = "na" selected = {this.state.current ==='na'?"selected":""}>Select a Country</option>
+                    <option value = "global" selected = {this.state.current === 'global'?"selected":""}>Global</option>
                     {
                         this.state.countries.Countries.map((data)=>{
-                             return <option>{data.Country}</option>
+                             return <option value = {data.CountryCode} selected = {this.state.current === data.CountryCode?"selected":""}>{data.Country}</option>
                         })
                     }
                 </select>
+                <div className = "table-country">
+                    <table style = {this.state.current == 'na'?{display: "none"}:{display:"block"}} className = "table">
+                        <tr>
+                            <th>Country</th>
+                            <th>Date</th>
+                            <th>New Confirmed Cases</th>
+                            <th>New Deaths</th>
+                            <th>New Recovered</th>
+                            <th>Total Confirmed Cases</th>
+                            <th>Total Deaths</th>
+                            <th>Total Recovered</th>
+                        </tr>
+                        <tr>
+                            <td>{this.state.global?"Global":this.state.countries.Countries[this.state.index].Country}</td>
+                            <td>{this.dateToText(this.state.countries.Date)}</td>
+                            <td>{this.state.global?this.state.countries.Global.NewConfirmed:this.state.countries.Countries[this.state.index].NewConfirmed}</td>
+                            <td>{this.state.global?this.state.countries.Global.NewDeaths:this.state.countries.Countries[this.state.index].NewDeaths}</td>
+                            <td>{this.state.global?this.state.countries.Global.NewRecovered:this.state.countries.Countries[this.state.index].NewRecovered}</td>
+                            <td>{this.state.global?this.state.countries.Global.TotalConfirmed:this.state.countries.Countries[this.state.index].TotalConfirmed}</td>
+                            <td>{this.state.global?this.state.countries.Global.TotalDeaths:this.state.countries.Countries[this.state.index].TotalDeaths}</td>
+                            <td>{this.state.global?this.state.countries.Global.TotalRecovered:this.state.countries.Countries[this.state.index].TotalRecovered}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         )
     }
